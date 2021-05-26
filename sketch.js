07bb9e90530126410551ec076dbcbc388887760b
Vi-ic1101 <83502402+Vi-ic1101@users.noreply.github.com>
2021-05-26 09:22:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,93 +1,76 @@
-var back1
-var back2
-
-var alien1
-var alien2
-
-var arrow
-var boy
-
-var gameOver
-var play
-var pause
-
-var spike1
-var spike2
-var sword
-
- var gold 
- var ruby
-
 var PLAY=1
 var END=0
+var PAUSE=2
 var gameState=PLAY
 
+var back
+var alien1,alien2
+var bow,arrow
+var gameOver,start,pause
+var sword,ruby,gold
+
 function preload(){
-back1Img=loadImage("back.png") ;
-
-    alien1Image=loadImage("alien1.png");
-    alien2Img=loadImage("alien2.png");
-
-    arrowImg=loadImage("arrow0.png")
-    bowImg=loadImage("bow0.png")
-
-    gameOverImg=loadImage("gameOver.png")
-    startImg=loadImage("start.png")
-    pauseImg=loadImage("pause.png")
+    backImg=loadImage("back.png")
+bowImg=loadImage("bow0.png")
+arrowImg=loadImage("arrow0.png")
+alien1Img=loadImage("alien1.png")
+alien2Img=loadImage("alien2.png")
+startImg=loadImage("start.png")
+pauseImg=loadImage("pause.png")
+gameOverImg=loadImage("gameOver.png")
+rubyImg=loadImage("ruby.png")
+goldImg=loadImage("gold.png")
+swordImg=loadImage("sword.png")
 }
 function setup(){
-    createCanvas(windowWidth,windowHeight);
-    back1=createSprite(200,200,20,40)
-    back1.addImage(back1Img);
-    back1.scale=4
-   
-   // back1.velocityX=10
-    
-//back2=createSprite(300,300,50,50);
-
-    alien1=createSprite(200,200,100,100);
-    alien1.addImage(alien1Image);
-    alien1.scale=0.09
-
-    alien2=createSprite(100,100);
-   // alien2.velocityX=-2
-    //alien2.lifetime=10
-    alien2.scale=0.1
-    alien2.addImage(alien2Img); 
-
-    arrow=createSprite(500,200)
+    createCanvas(windowWidth,windowHeight)
+    back=createSprite(windowWidth/2,windowHeight/2,windowWidth,windowHeight)
+    back.scale=10
+    back.addImage(backImg)
+    bow=createSprite(200,200,50,50)
+    bow.addImage(bowImg)
+    arrow=createSprite(200,100,50,50)
     arrow.addImage(arrowImg)
-
-    bow=createSprite(300,300);
-    bow.addImage(bowImg);
-
-    gameOver=createSprite(400,400)
-    gameOver.addImage(gameOverImg)
-
-    start=createSprite(500,40+10+10)
-    start.addImage(startImg)
-
-    pause=createSprite(600,300)
+    arrow.debug=true
+    arrow.velocityX=2
+    alien1=createSprite(600,100,50,50)
+    alien1.addImage(alien1Img)
+    alien1.scale=0.2
+  //  alien1.velocityX=-4
+alien1.destroy=100
+    alien2=createSprite(200,400,50,50)
+    alien2.addImage(alien2Img)
+    alien2.scale=0.2
+    start=createSprite(200,500,50,50)
+   start.addImage(startImg)
+   pause=createSprite(200,600,50,50)
     pause.addImage(pauseImg)
-
+    gameOver=createSprite(200,700,50,50)
+    gameOver.addImage(gameOverImg)
+    sword=createSprite(200,300,50,50)
+    sword.addImage(swordImg)
+    sword.scale=0.2
+    gold=createSprite(600,200,50,50)
+    gold.addImage(goldImg)
+    gold.scale=0.3
+    ruby=createSprite(600,300,50,50)
+    ruby.addImage(rubyImg)
+    ruby.scale=0.1
 }
-function draw() {
-    background(0);
-    drawSprites();
-    if(back1.x > windowWidth){
-       back1.x=1
-    }
-
+ function draw(){
+     console.log(gameState)
+    background(0)
+     drawSprites();
+     //alien1.debug=true
 if(gameState===PLAY){
-back1.velocityX=11
-
-}else if(gameState===End){
-
+    
+if(arrow.isTouching(alien1)){
+    gameState=END
+    arrow.velocityX=0
+ }
+ //spawnAlien();
+}else if(gameState===END){}
 }
-}
-function alien1(){}
-function alien2(){}
-function bow(){}
 
-
-        
+function spawnAlien(){
+    }
